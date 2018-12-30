6ee95ade88f29e1761be130092cb1d05e1f30244
Kostya <a1@humster.local>
2018-12-30 04:55:42 +0200

    Back

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import createBrowserHistory from 'history/createBrowserHistory.js'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
 const initialState = {}
 
