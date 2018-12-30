4bb36a391b862ffdb29b82b263f915b0dbd59ce4
Kostya <a1@humster.local>
2018-12-30 03:33:03 +0200

    Removed redux

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
-import thunk from 'redux-thunk'
+// import thunk from 'redux-thunk'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { routerMiddleware } from 'react-router-redux'
+// import { routerMiddleware } from 'react-router-redux'
 
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
+// import { createStore, applyMiddleware } from 'redux'
+// import { Provider } from 'react-redux'
 
-import reducer, { history } from './reducers/index'
+// import reducer, { history } from './reducers/index'
 import { ParallaxProvider } from 'react-scroll-parallax'
 
-const store = createStore(
-  reducer,
-  applyMiddleware(routerMiddleware(history), thunk)
-)
+// const store = createStore(
+//   reducer,
+//   applyMiddleware(routerMiddleware(history), thunk)
+// )
 
 export const Application = () => {ReactDOM.render(
   <ParallaxProvider>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <Router>
         <Switch>
           <Route path="/">
@@ -27,11 +27,11 @@ export const Application = () => {ReactDOM.render(
           </Route>
         </Switch>
       </Router>
-    </Provider>
+    {/* </Provider> */}
   </ParallaxProvider>,
   document.getElementById('root')
 )}
 
-store.subscribe(Application)
+// store.subscribe(Application)
 
 Application()
